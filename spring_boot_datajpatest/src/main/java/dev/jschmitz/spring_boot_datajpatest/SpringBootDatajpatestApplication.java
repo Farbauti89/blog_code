70cbc8187950f8e5f2fb2917689a391b91e6fa6f
Jan Schmitz <jan.schmitz@entity.de>
2021-03-10 09:05:13 +0100

    adds new module
    for datajpatest blogpost

---
+++ spring_boot_datajpatest/src/main/java/dev/jschmitz/spring_boot_datajpatest/SpringBootDatajpatestApplication.java
@@ -0,0 +1,13 @@
+package dev.jschmitz.spring_boot_datajpatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatajpatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatajpatestApplication.class, args);
+    }
+
+}
---
+++ spring_boot_datajpatest/src/test/java/dev/jschmitz/spring_boot_datajpatest/SpringBootDatajpatestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jschmitz.spring_boot_datajpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatajpatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
